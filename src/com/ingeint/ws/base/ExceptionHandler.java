c5aef448bcf08a4fe1a9678d2761b17a2db6b1cb
Saúl Piña <saul.pina@ingeint.com>
2019-08-15 19:58:40 -0500

    Update exception handler

--- src/com/ingeint/ws/base/ExceptionHandler.java
+++ src/com/ingeint/ws/base/ExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 
 import org.adempiere.exceptions.AdempiereException;
+import org.compiere.util.Trx;
 
 import com.ingeint.ws.presenter.ExceptionMessage;
 
@@ -11,6 +12,8 @@ public class ExceptionHandler implements ExceptionMapper<AdempiereException> {
 
 	@Override
 	public Response toResponse(AdempiereException exception) {
+		Trx trx = Trx.get(RequestEnv.getCurrentTrxName(), false);
+		trx.rollback();
 		return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new ExceptionMessage(Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(), exception.getMessage())).build();
 	}
 
